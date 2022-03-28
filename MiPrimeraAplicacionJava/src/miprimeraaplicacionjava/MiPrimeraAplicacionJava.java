a04b9cab052e42d7289c94cf8da3821d461bfca8
markgarcia1011 <101343926+markgarcia1011@users.noreply.github.com>
2022-03-28 11:23:54 -0600

    Add files via upload

---
+++ MiPrimeraAplicacionJava/src/miprimeraaplicacionjava/MiPrimeraAplicacionJava.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraaplicacionjava;
+
+/**
+ *
+ * @author ITCA
+ */
+public class MiPrimeraAplicacionJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declarar variables (ejemplos de algunos tipos de datos)
+        String NombreCompleto,Direccion,estadocivil;
+        String direccion = "San Salvador";
+        int edad;
+        double Gastosdiarios;
+        char genero;
+        boolean poseenVehiculo;
+        //Inicializacion de variables
+        NombreCompleto = "Camila Esperanza Rooslers";
+        Direccion = "Calle de la amagura desvio la tristeza";
+        estadocivil = "Soltera";
+        edad = 30;
+        Gastosdiarios = 34,45;
+        genero = "F";
+        poseenVehiculo = "False";
+        
+        //Tipo de variables (Mostrar en pantalla las variables)
+        System.out.println("Nombre completo: " + nombreCompleto);
+            System.out.println("Direccion: "+ direccion);
+        System.out.println("Estadpo Civil: "+ estadocivil);
+         System.out.println("Edad: "+ edad);
+          System.out.println("Gastos Diarios: "+ gastodiarios);
+           System.out.println("Genero: "+ genero);
+            System.out.println("Pose Vehiculo: "+ poseevehiculo);
+    }
+    
+}
